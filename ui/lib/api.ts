83948293727706ce267976d86de260e2cf14bce0
ttadesse627 <ttadesse627@gmail.com>
2025-12-25 20:56:30 +0300

    removed comment

--- ui/lib/api.ts
+++ ui/lib/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-//Set the api base URL from environment variables for test
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
 });
