2be62cc5edfed996ca0a767c4b84fa23614b2945
ttadesse627 <ttadesse627@gmail.com>
2025-12-25 20:45:56 +0300

    updated workflow

--- ui/lib/api.ts
+++ ui/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-//Set the api base URL from environment variable
+//Set the api base URL from environment variables for test
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
 });
